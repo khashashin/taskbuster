ba1fdd6179af943aac0b10678fdc26e22597b7ad
khashashin <khashashin@gmail.com>
2018-01-15 21:06:02 +0100

    checking

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.path.abspath
 from django.conf import settings
 settings.configure()
 

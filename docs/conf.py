35d4bf44405feb4fceb17c742c2f0132424dc595
khashashin <khashashin@gmail.com>
2018-01-15 20:59:06 +0100

    path fixes

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.dirname(os.path.abspath('.')))
+from django.conf import settings
+settings.configure()
+
 
 # -- General configuration ------------------------------------------------
 
